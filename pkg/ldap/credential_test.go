db26bd259ca9c1921e8b4d828d13fa5755397a96
Sebastiaan Mannem <sebas@mannem.nl>
2025-04-04 13:58:18 +0200

    Adding unittests

---
+++ pkg/ldap/credential_test.go
@@ -0,0 +1,49 @@
+package ldap_test
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/pgvillage-tools/pgfga/pkg/ldap"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCredential(t *testing.T) {
+	const myFirstValue = "myval1"
+	const mySecondValue = "myval2"
+	var myBase64EncryptedValue = base64.StdEncoding.EncodeToString([]byte(mySecondValue))
+	tmpDir, err := os.MkdirTemp("", "Credential")
+	if err != nil {
+		panic(fmt.Errorf("unable to create temp dir: %w", err))
+	}
+	defer os.RemoveAll(tmpDir)
+	myCredFile := path.Join(tmpDir, "my-creds-file")
+	require.NoError(t, os.WriteFile(myCredFile, []byte(myFirstValue), 0o0600))
+	myB64CredFile := path.Join(tmpDir, "my-b64-creds-file")
+	require.NoError(t, os.WriteFile(myB64CredFile, []byte(myBase64EncryptedValue), 0o0600))
+	for _, test := range []struct {
+		value    string
+		file     string
+		base64   bool
+		expected string
+	}{
+		{value: myFirstValue, expected: myFirstValue},
+		{file: myCredFile, expected: myFirstValue},
+		{value: myBase64EncryptedValue, base64: true, expected: mySecondValue},
+		{file: myB64CredFile, base64: true, expected: mySecondValue},
+	} {
+		t.Logf("test values: %v", test)
+		cred := ldap.Credential{
+			Value:  test.value,
+			File:   test.file,
+			Base64: test.base64,
+		}
+		myCred, err := cred.GetCred()
+		assert.NoError(t, err)
+		assert.Equal(t, test.expected, myCred)
+	}
+}
